a1391b32d8d6cb4fd08272060ad73a993ac5dd08
vignesh <vignesh@Swethas-MacBook-Pro.local>
2020-07-05 09:35:10 -0700

    Added View Controller

--- EmojiDictionary/ViewController.swift
+++ EmojiDictionary/ViewController.swift
@@ -1,23 +1,37 @@
-//
-//  ViewController.swift
-//  EmojiDictionary
-//
+
 //  Created by vignesh on 7/4/20.
-//  Copyright © 2020 AmVi. All rights reserved.
-//
-//
+//To add emojis control+Command+ space bar.😂
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var emojis = ["😀","😬","😆","🤑"]
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+      //Howmany rows going to be in a table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+
+
+    //WHat is the content in the table view (cell =table row)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Creating new UI Table view cell abd assiging it to cell constant.
+        //let keyword is to declare contstant.
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
 
 
